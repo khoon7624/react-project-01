c7884cbdadec7ee1028dd1ae1361a3202ad93098
khoon <khoon7624@gmail.com>
2023-01-30 16:08:08 +0900

    칠드런 다시

---
+++ src/components/Card.js
@@ -0,0 +1,6 @@
+import "./Card.css";
+
+function Card(props) {
+  return <div ClassName="card">{props.children}</div>;
+}
+export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate'
+import Card from "./Card";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props){
-
-    return(
-        <div className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </div>
-    )
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
